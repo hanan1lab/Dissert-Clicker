1e5b2fe11cd537d5f3d82cb14a30f4c12c972e8f
Hanan labib <hananlabib99@gmail.com>
2024-12-12 10:05:24 +0700

    NIM 234311041

---
+++ app/src/main/java/com/hanan/dessertclicker/data/DessertUiState.kt
@@ -0,0 +1,12 @@
+package com.hanan.dessertclicker.data
+
+import androidx.annotation.DrawableRes
+import com.hanan.dessertclicker.data.Datasource.dessertList
+
+data class DessertUiState(
+    val currentDessertIndex: Int = 0,
+    val dessertsSold: Int = 0,
+    val revenue: Int = 0,
+    val currentDessertPrice: Int = dessertList[currentDessertIndex].price,
+    @DrawableRes val currentDessertImageId: Int = dessertList[currentDessertIndex].imageId
+)
